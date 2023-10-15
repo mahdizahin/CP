3ab26691dc50170adaf31859838bae42d7d59bd0
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2023-10-16 00:13:05 +0600

    Add files via upload

---
+++ D1/001.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int x;
+cout<<"Enter integer contains 1-100"<<endl;
+cin>>x;
+
+
+if(x==8){
+    cout<<"YES";
+}else{
+cout<<"Enter again";
+}
+
+
+}
